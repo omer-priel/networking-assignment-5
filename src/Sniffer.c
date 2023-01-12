e716b6555b3a602b8cce0e11cdd969c1ac969732
Omer Priel <omer.priel2@gmail.com>
2023-01-12 13:50:34 +0200

    wip

--- src/Sniffer.c
+++ src/Sniffer.c
@@ -1,5 +1,85 @@
 #include "api.h"
 
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
+#include <stdio.h>
+#include <pcap.h>
+#include <stdio.h>
+#include <pcap.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+/* Ethernet header */
+struct ethheader
+{
+    u_char ether_dhost[ETHER_ADDR_LEN]; /* destination host address */
+    u_char ether_shost[ETHER_ADDR_LEN]; /* source host address */
+    u_short ether_type;                 /* IP? ARP? RARP? etc */
+};
+
+/* IP Header */
+struct ipheader
+{
+    unsigned char iph_ihl : 4,       // IP header length
+        iph_ver : 4;                 // IP version
+    unsigned char iph_tos;           // Type of service
+    unsigned short int iph_len;      // IP Packet length (data + header)
+    unsigned short int iph_ident;    // Identification
+    unsigned short int iph_flag : 3, // Fragmentation flags
+        iph_offset : 13;             // Flags offset
+    unsigned char iph_ttl;           // Time to Live
+    unsigned char iph_protocol;      // Protocol type
+    unsigned short int iph_chksum;   // IP datagram checksum
+    struct in_addr iph_sourceip;     // Source IP address
+    struct in_addr iph_destip;       // Destination IP address
+};
+
+void got_packet(const struct pcap_pkthdr *header,
+                const u_char *packet)
+{
+    struct ethheader *eth = (struct ethheader *)packet;
+
+    if (ntohs(eth->ether_type) == 0x0800)
+    { // 0x0800 is IP type
+        struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
+
+        printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));
+        printf("         To: %s\n", inet_ntoa(ip->iph_destip));
+
+        /* determine protocol */
+        switch (ip->iph_protocol)
+        {
+        case IPPROTO_TCP:
+            printf("   Protocol: TCP\n");
+            return;
+        case IPPROTO_UDP:
+            printf("   Protocol: UDP\n");
+            return;
+        case IPPROTO_ICMP:
+            printf("   Protocol: ICMP\n");
+            return;
+        default:
+            printf("   Protocol: others\n");
+            return;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     char *filter = argv[1];
@@ -43,7 +123,10 @@ int main(int argc, char *argv[])
     {
         packet = pcap_next(handle, &header);
 
-        printf("packet: %s\n", packet);
+        if (header.len > 0)
+        {
+            got_packet(&header, packet);
+        }
     }
 
     pcap_close(handle);
